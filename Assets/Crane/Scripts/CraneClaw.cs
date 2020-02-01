87838bc0a2ea87728f037cdd299121ff6d02e045
Technostalgic <Isaiahnsmith@hotmail.com>
2020-02-01 02:04:26 -0700

    organized crane parts to prefab

---
+++ Assets/Crane/Scripts/CraneClaw.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraneClaw : MonoBehaviour
+{
+	[SerializeField]
+	private Transform _rightClaw;
+
+	[SerializeField]
+	private Transform _leftClaw;
+
+	public bool isOpen { get; set; }
+}
