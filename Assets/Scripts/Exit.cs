1d14a1ec23ea0fe91aad789972d4a6c3aff469be
Josh Killinger <josh.killinger@gmail.com>
2020-02-01 23:53:56 -0700

    Adding Exit script.

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    public void Event_OnClick()
+    {
+#if UNITY_EDITOR
+        Debug.Log("Exit");
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit(0);
+        #endif
+    }
+}
