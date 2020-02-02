bc5cfa6763da8d80b5578f04e4698fb0f95c417c
Josh Killinger <josh.killinger@gmail.com>
2020-02-01 21:54:01 -0700

    Fixing input test script.

--- Assets/TestingInput.cs
+++ Assets/TestingInput.cs
@@ -12,13 +12,15 @@ public class TestingInput : MonoBehaviour
     public float p2v;
     public float p2g;
 
+    public bool gamepad = true;
+
     private void Update()
     {
-        p1h = Input.GetAxis("Player1_Horizontal");
-        p1v = Input.GetAxis("Player1_Vertical");
+        p1h = Input.GetAxis($"Player1_Horizontal_{(gamepad ? "G" : "K")}");
+        p1v = Input.GetAxis($"Player1_Vertical_{(gamepad ? "G" : "K")}");
         p1g = Input.GetAxis("Player1_Grab");
-        p2h = Input.GetAxis("Player2_Horizontal");
-        p2v = Input.GetAxis("Player2_Vertical");
+        p2h = Input.GetAxis($"Player2_Horizontal_{(gamepad ? "G" : "K")}");
+        p2v = Input.GetAxis($"Player2_Vertical_{(gamepad ? "G" : "K")}");
         p2g = Input.GetAxis("Player2_Grab");
     }
 
